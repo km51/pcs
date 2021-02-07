3baff85c1149ae942eb50a53ba4005257efe3c44
km51 <56459004+km51@users.noreply.github.com>
2021-02-08 07:10:19 +0900

    Update index.php

--- index.php
+++ index.php
@@ -90,5 +90,7 @@ if (isset($_SESSION['member_id']) && $_SESSION['time'] + 3600 > time()){
 </div>
 
 <script src="js/jquery-3.3.1.min.js"></script>
+<script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+
 </body>
 </html>
