f982e1febf142583f36fc271a13540965f340aef
km51 <56459004+km51@users.noreply.github.com>
2021-02-08 06:57:54 +0900

    Update reportEdit.php

--- reportEdit.php
+++ reportEdit.php
@@ -347,5 +347,6 @@ if($_REQUEST['action'] == 'rewrite' && isset($_SESSION['join'])){
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 <script type="text/javascript" src="js/instant.js"></script>
+	<script src="js/jquery-3.3.1.min.js"></script>
 </body>
 </html>
